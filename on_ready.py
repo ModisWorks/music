776cfb1eac37e714ad67cee3aaf804c2fc7fa003
Infraxion <Infraxion@users.noreply.github.com>
2019-07-09 15:49:23 +1000

    Removed extra file

--- on_ready.py
+++ on_ready.py
@@ -4,5 +4,4 @@ from . import _musicplayer, _data
 
 async def on_ready():
     # _musicplayer.clear_cache_root()
-    for voice_client in main.client.voice_clients:
-        await voice_client.disconnect(force=True)
+    pass
